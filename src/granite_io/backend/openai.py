013cfeaa8e207a7a41a54c28a589ce3989e2e99c
Mark Sturdevant <mark.sturdevant@ibm.com>
2025-02-27 12:30:47 -0800

    Use factories, remove unwanted stuff
    * Have to use factories for things to work right for reasons.
    * We are not consistently using the conftest flags. Removed.
    * No longer use chat/completions because we like generate --> completion
    * env vars are usable to point tests (e.g. openai server like RITS)
      - but a config like MODEL_NAME would probably work for transformers or
        openai and not both (i.e. they don't like the same name).
    
    Signed-off-by: Mark Sturdevant <mark.sturdevant@ibm.com>

--- src/granite_io/backend/openai.py
+++ src/granite_io/backend/openai.py
@@ -1,16 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Standard
-import os
 
 # Third Party
 from openai import OpenAI
 import aconfig
 
 # Local
-from granite_io.backend.base import Backend, ChatCompletionBackend
+from granite_io.backend.base import Backend
 from granite_io.backend.registry import backend
-from granite_io.types import ChatCompletionInputs, GenerateResult
+from granite_io.types import GenerateResult
 
 
 @backend(
@@ -21,54 +20,24 @@ from granite_io.types import ChatCompletionInputs, GenerateResult
             "openai_base_url": {"type": "string"},
             "openai_api_key": {"type": "string"},
         }
-    }
+    },
 )
-class OpenAIBackend(Backend, ChatCompletionBackend):
+class OpenAIBackend(Backend):
     _model_str: str
     _openai_client: OpenAI
 
     def __init__(self, config: aconfig.Config):
         self._model_str = config.model_name
-        api_key = config.openai_api_key or os.environ.get("OPENAI_API_KEY", "ollama")
-        base_url = config.openai_base_url or os.environ.get("OPENAI_BASE_URL", "http://localhost:11434/v1")
+        api_key = config.get("openai_api_key", "ollama")
+        base_url = config.get("openai_base_url", "http://localhost:11434/v1")
+
+        default_headers = {"RITS_API_KEY": api_key} if api_key else None
 
         self.openai_client = OpenAI(
             base_url=base_url,
             api_key=api_key,
-            default_headers={
-                "RITS_API_KEY": api_key,  # Does this extra header break anything?
-                "OPEN_API_KEY": api_key,
-            },
+            default_headers=default_headers,
         )
-
-    def create_chat_completion(self, input_chat: ChatCompletionInputs) -> str:
-        messages = [{"role": x.role, "content": x.content} for x in input_chat.messages]
-
-        result = self.openai_client.chat.completions.create(
-            model=self._model_str,
-            messages=messages,
-            store=True,
-        )
-
-        raw_message = result.choices[0].message
-
-        if raw_message.role != "assistant":
-            raise ValueError(f"Unexpected role '{raw_message.role}' in chat completion")
-        if raw_message.content is None:
-            raise NotImplementedError(
-                f"No text in chat completion, and decoding of other types is not "
-                f"implemented. Completion result: {raw_message}"
-            )
-
-        # # return before_state.append(AssistantMessage(raw_message.content))
-        # TODO: don't we want this stuff too?
-        # return GenerateResult(
-        # completion_string=raw_message.content,
-        # completion_tokens=result.usage.completion_tokens,
-        # stop_reason=result.choices[0].finish_reason
-        # )
-
-        return raw_message.content
 
     def generate(self, input_str: str) -> GenerateResult:
         """Run a direct /completions call"""
