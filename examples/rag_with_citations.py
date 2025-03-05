f865f0d4c5babf4651b841e5c8f2e056f065a11e
Martin Hickey <martin.hickey@ie.ibm.com>
2025-03-05 16:17:49 +0000

    Add example python script
    Script tests the parser capability by calling
    Granite model with RAG and citations.
    
    Closes #36
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

---
+++ examples/rag_with_citations.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+This example show how to infer or call a model using the framework
+and an Ollama backend to serve the model.
+In this scenario, it is a simple RAG use case where citations are enabled.
+"""
+
+# Local
+from granite_io import make_backend, make_io_processor
+from granite_io.types import ChatCompletionInputs, UserMessage
+from granite_io.io.granite_3_2 import _Document
+
+import model_output_parser
+
+model_name = "granite3.2:8b"
+io_processor = make_io_processor(
+    model_name, backend=make_backend("openai", {"model_name": model_name})
+)
+question = "What is RAG?"
+messages = [UserMessage(content=question)]
+
+documents = [
+    {"text": "Dynamic Data: Automatically generate and refine graphs to best fit your domain and ontology needs."}, 
+    {"text": "RAG, retrieval-augmented generation, is a technique that grants generative artificial intelligence models information retrieval capabilities."},
+]
+
+# With RAG and citations
+outputs = io_processor.create_chat_completion(
+    ChatCompletionInputs(messages=messages, documents=documents, controls={"citations": True})
+)
+print(">> Response:")
+response = outputs.next_message.content
+print(response)
+
+print(">> Extract response into text and citation categories:")
+response_separated_by_citation = outputs.next_message.content.split("# Citations:")
+if len(response_separated_by_citation) == 2:
+    model_output_parser.parse_output(response, response_separated_by_citation[1])
+else:
+    raise RuntimeError(f"Unable to separate reponse by citation: {response_separated_by_citation}")
