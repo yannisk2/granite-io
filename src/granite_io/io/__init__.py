a41582b778c60ef698738be49e3269f2c409c83a
Martin Hickey <martin.hickey@ie.ibm.com>
2025-03-10 20:39:42 +0000

    Update after review
    Review comments:
    - https://github.com/ibm-granite/granite-io/pull/51#pullrequestreview-2668706626
    
    Updates:
    - Updated Citations, Documents and Hallucination fields so they they will be
    only be serialized with AssistantMessage object if set.
    - Moved testdata directory to specific granite_3_2 directory to self contain
    the tests data
    - Moved Granite 3.2 specific implementation (io processor and parser) to
    Granite 3.2 module under IO
    - Modify the parser to log errors and return gracefully to the caller rather
    than exceptions
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    Signed-off-by: Louis Mandel <lmandel@us.ibm.com>

--- src/granite_io/io/__init__.py
+++ src/granite_io/io/__init__.py
@@ -8,4 +8,4 @@ processing.
 # Local
 from granite_io.io.base import InputOutputProcessor  # noqa: F401
 from granite_io.io.registry import io_processor, make_io_processor  # noqa: F401
-import granite_io.io.granite_3_2  # noqa: F401
+import granite_io.io.granite_3_2.granite_3_2  # noqa: F401
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 # Local
 from granite_io import make_backend
 from granite_io.backend import Backend
-from granite_io.io.granite_3_2 import GRANITE_3_2_2B_HF
+from granite_io.io.granite_3_2.granite_3_2 import GRANITE_3_2_2B_HF
 
 
 @pytest.fixture(scope="session")
