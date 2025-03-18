15c9def86ee3852794e2bbfced7f02f9cb3e36d0
Martin Hickey <martin.hickey@ie.ibm.com>
2025-03-18 17:16:40 +0000

    Export input and output processors
    Enable the input and output processors to be usable
    from the library independent of the IO processors.
    
    This PR exposes the Granite 3.w input and output
    processors.
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- src/granite_io/__init__.py
+++ src/granite_io/__init__.py
@@ -7,4 +7,11 @@ the advanced control instructions with Granite models.
 
 # Local
 from granite_io.backend import backend, make_backend  # noqa: F401
-from granite_io.io import io_processor, make_io_processor  # noqa: F401
+from granite_io.io import (  # noqa: F401
+    get_input_processor,
+    get_output_processor,
+    input_processor,
+    io_processor,
+    make_io_processor,
+    output_processor,
+)
--- src/granite_io/io/registry.py
+++ src/granite_io/io/registry.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """
-Registry of all IO Processors
+Registry of all Input, Output & IO Processors
 """
 
 # Local
@@ -10,3 +10,11 @@ from granite_io.factory import ImportableFactory
 _IO_FACTORY = ImportableFactory("io")
 io_processor = _IO_FACTORY.decorator
 make_io_processor = _IO_FACTORY.construct
+
+_INPUT_FACTORY = ImportableFactory("input")
+input_processor = _INPUT_FACTORY.decorator
+get_input_processor = _INPUT_FACTORY.construct
+
+_OUTPUT_FACTORY = ImportableFactory("output")
+output_processor = _OUTPUT_FACTORY.decorator
+get_output_processor = _OUTPUT_FACTORY.construct
