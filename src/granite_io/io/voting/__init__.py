27314f3eab81b20c7e9c94a6c2c1986b4fa6b729
Martin Hickey <martin.hickey@ie.ibm.com>
2025-04-02 13:31:16 +0100

    Remove MajorityVotingProcessor processor
    The MajorityVotingProcessor processor is replaced
    with adding majority voting into existing processors. It uses a flag
    instead like reasoning whioch encapsulates the implementation
    from the user.
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- src/granite_io/io/voting/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-The voting module holds I/O processors that perform various types of majority voting.
-"""
-
-# Local
-from granite_io.io.voting.voting import (  # noqa: F401
-    MajorityVotingProcessor,
-    integer_normalizer,
-)
