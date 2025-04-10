4222f096c6544a54ea82dae2095e5a8eef6a05cf
Martin Hickey <martin.hickey@ie.ibm.com>
2025-04-10 11:20:40 +0100

    Updates for feedback in PR #115
    PR #115 was merged and these are addressing some small nits that didn't
    make it into the merge. Review comments:
    
    https://github.com/ibm-granite/granite-io/pull/115#pullrequestreview-2754844705
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- src/granite_io/io/voting/__init__.py
+++ src/granite_io/io/voting/__init__.py
@@ -3,8 +3,8 @@ The voting module holds I/O processors that perform various types of majority vo
 """
 
 # Local
-from granite_io.io.voting.mbrd_voting import MBRDMajorityVotingProcessor  # noqa: F401
-from granite_io.io.voting.voting import (  # noqa: F401
+from granite_io.io.voting.mbrd import MBRDMajorityVotingProcessor  # noqa: F401
+from granite_io.io.voting.simple import (  # noqa: F401
     MajorityVotingProcessor,
     integer_normalizer,
 )
