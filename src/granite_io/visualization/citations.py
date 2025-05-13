65fbb2fadc8d9466fbc0777d5557bf8f73264469
Fred Reiss <frreiss@us.ibm.com>
2025-05-13 16:18:19 -0700

    Fix linter issues
    Signed-off-by: Fred Reiss <frreiss@us.ibm.com>

--- src/granite_io/visualization/citations.py
+++ src/granite_io/visualization/citations.py
@@ -14,22 +14,28 @@ from granite_io.types import (
     ChatCompletionResults,
 )
 
-class CitationsWidget():
+
+class CitationsWidget:
     def __init__(self):
         super().__init__()
-             
+
     def show(self, inputs: ChatCompletionInputs, outputs: ChatCompletionResults):
-        documents = [ doc.model_dump(mode='json') for doc in inputs.documents ]
+        documents = [doc.model_dump(mode="json") for doc in inputs.documents]
         response = outputs.results[0].next_message.content
-        citations = [ citation.model_dump(mode='json') for citation in outputs.results[0].next_message.citations ]
-        
-        return CitationsWidgetInstance(response=response, documents=documents, citations=citations)
-    
-    
+        citations = [
+            citation.model_dump(mode="json")
+            for citation in outputs.results[0].next_message.citations
+        ]
+
+        return CitationsWidgetInstance(
+            response=response, documents=documents, citations=citations
+        )
+
+
 class CitationsWidgetInstance(anywidget.AnyWidget):
-    _esm = pathlib.Path(__file__).parent/"index.js"
-    _css = pathlib.Path(__file__).parent/"index.css"
-    
+    _esm = pathlib.Path(__file__).parent / "index.js"
+    _css = pathlib.Path(__file__).parent / "index.css"
+
     response = traitlets.Unicode({}).tag(sync=True)
     documents = traitlets.List([]).tag(sync=True)
     citations = traitlets.List([]).tag(sync=True)
