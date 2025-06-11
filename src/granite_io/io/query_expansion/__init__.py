cc95f1f3ae65c1f91067fd7e72ff0a998acde25a
vraj <vraj@ibm.com>
2025-06-11 23:26:48 +0000

    update init file
    Signed-off-by: vraj <vraj@ibm.com>

--- src/granite_io/io/query_expansion/__init__.py
+++ src/granite_io/io/query_expansion/__init__.py
@@ -4,7 +4,7 @@
 # Local
 from .query_expansion import QueryExpansionIOProcessor
 
-# Expose public symbols at `granite_io.io.hallucinations` to save users from typing
+# Expose public symbols at `granite_io.io.query_expansion` to save users from typing
 __all__ = [
     "QueryExpansionIOProcessor",
 ]
